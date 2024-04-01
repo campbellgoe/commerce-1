f4e2166f6033b9ee12828ea5779fcd2460e3b4c7
George Campbell <campbell.goe@gmail.com>
2024-04-01 17:42:28 +0100

    show price range

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -5,12 +5,25 @@ import { Product } from 'lib/shopify/types';
 import { VariantSelector } from './variant-selector';
 
 export function ProductDescription({ product }: { product: Product }) {
+  const priceHasRange =
+    product.priceRange.maxVariantPrice.amount > product.priceRange.minVariantPrice.amount;
   return (
     <>
       <div className="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
         <h1 className="mb-2 text-5xl font-medium">{product.title}</h1>
         <div className="mr-auto w-auto rounded-full bg-blue-600 p-2 text-sm text-white">
+          {priceHasRange && (
+            <>
+              <Price
+                className="inline-block"
+                amount={product.priceRange.minVariantPrice.amount}
+                currencyCode={product.priceRange.maxVariantPrice.currencyCode}
+              />
+              {' - '}
+            </>
+          )}
           <Price
+            className="inline-block"
             amount={product.priceRange.maxVariantPrice.amount}
             currencyCode={product.priceRange.maxVariantPrice.currencyCode}
           />
